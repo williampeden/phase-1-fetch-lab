a5f9a5e7d0c246388c44d5f8ad889d12effc7d7b
William <wgepeden@gmail.com>
2022-02-12 20:52:07 -0500

    commit tree 61d48929ab99ca17da2986e8884342f74897a5f1

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -13,5 +17,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
